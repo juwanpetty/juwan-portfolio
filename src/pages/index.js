1b3f2efd0a1c6ace7553ce8d4157f4010d25bdd7
juwanpetty <jchpetty@gmail.com>
2020-03-29 23:08:43 -0400

    Remove unnused code (image) from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import SEO from "../components/seo"
 import { Content } from "../components/Layout"
 import { Heading, Projects, Posts } from "../components/Index"
-// import Image from "../components/image"
 
 const IndexPage = () => (
   <Content>
@@ -11,9 +10,6 @@ const IndexPage = () => (
     <Heading />
     <Projects />
     <Posts />
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> */}
   </Content>
 )
 
