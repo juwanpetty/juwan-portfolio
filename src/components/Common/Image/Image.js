4f7ca926014a013e68da0d1fbba5c6031b70ac34
juwanpetty <jchpetty@gmail.com>
2020-03-29 23:09:48 -0400

    Add Image and TextAside component to Common

---
+++ src/components/Common/Image/Image.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  margin: 96px auto;
+`
---
+++ src/components/Common/TextAside/TextAside.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const TextAside = styled.div`
+  display: grid;
+  grid-gap: 48px;
+  grid-template-columns: 1fr 1fr;
+
+  img {
+    margin: 0;
+    align-self: center;
+  }
+
+  h2 {
+    margin-bottom: 16px;
+  }
+`
