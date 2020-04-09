8decc0c50aa14bcec98ee9fae2f4ed3d1c9e7bc0
juwanpetty <jchpetty@gmail.com>
2020-04-08 23:13:23 -0400

    Added dynamic posts to Posts.js

--- src/components/Index/Posts/Posts.js
+++ src/components/Index/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import {
   Wrapper,
   PostList,
@@ -10,35 +11,62 @@ import {
 } from "./Posts.module"
 import { TwoColumn, HeaderText, Paragraph, LinkText } from "../../Common"
 
-export const Posts = () => {
-  return (
-    <Wrapper>
-      <TwoColumn>
-        <HeaderText>Recent posts</HeaderText>
-        <div>
-          <Paragraph>
-            I enjoy taking some time to think about how we work, design and
-            live, whenever possible. Here are some of my more recent stories and
-            ideas I’ve published.
-          </Paragraph>
-          <LinkText to="/blog">See all posts</LinkText>
-        </div>
-      </TwoColumn>
+export const Posts = () => (
+  <StaticQuery
+    query={graphql`
+      query PostsQuery {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 3
+        ) {
+          edges {
+            node {
+              id
+              excerpt
+              frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                category
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Wrapper>
+        <TwoColumn>
+          <HeaderText>Recent posts</HeaderText>
+          <div>
+            <Paragraph>
+              I enjoy taking some time to think about how we work, design and
+              live, whenever possible. Here are some of my more recent stories
+              and ideas I’ve published.
+            </Paragraph>
+            <LinkText to="/blog">See all posts</LinkText>
+          </div>
+        </TwoColumn>
 
-      <PostList>
-        <Post>
-          <Title to="/blog">
-            <h3>Understanding Generators in JavaScript</h3>
-          </Title>
-          <PostDate>
-            March 2020 - <Tag>Development</Tag>
-          </PostDate>
-          <Description>
-            A leader can be the head of a household, the head of a classroom,
-            the head of company, and yes, the head of a country.
-          </Description>
-        </Post>
-      </PostList>
-    </Wrapper>
-  )
-}
+        <PostList>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Post key={node.id}>
+              <Title to={node.fields.slug}>
+                <h3>{node.frontmatter.title}</h3>
+              </Title>
+              <PostDate>
+                {node.frontmatter.date} -{" "}
+                <Tag to={`/blog/${node.frontmatter.category.toLowerCase()}`}>
+                  {node.frontmatter.category}
+                </Tag>
+              </PostDate>
+              <Description>{node.excerpt}</Description>
+            </Post>
+          ))}
+        </PostList>
+      </Wrapper>
+    )}
+  />
+)
